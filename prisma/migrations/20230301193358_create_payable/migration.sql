adde2419b229a4ca1db0eadd15aa63e21c80e132
Lucas Duarte <lucs.rkt@gmail.com>
2023-03-01 16:40:27 -0300

    :sparkles: Create payable model

---
+++ prisma/migrations/20230301193358_create_payable/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE "Transaction" ALTER COLUMN "creditCardNumber" SET DATA TYPE BIGINT;
+
+-- CreateTable
+CREATE TABLE "Payable" (
+    "id" SERIAL NOT NULL,
+    "status" TEXT NOT NULL,
+    "fee" DOUBLE PRECISION NOT NULL,
+    "dueDate" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Payable_pkey" PRIMARY KEY ("id")
+);
