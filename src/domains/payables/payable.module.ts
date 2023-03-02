767b16bdb1cd4ba5bb3cb33e597b603b85d61a02
Lucas Duarte <lucs.rkt@gmail.com>
2023-03-02 20:03:08 -0300

    :fire: Remove payable controller

--- src/domains/payables/payable.module.ts
+++ src/domains/payables/payable.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
 import { PrismaPayableRepository } from 'src/repositories/prisma/prisma-payable-repository';
 import { PayableRepository } from 'src/repositories/payable-repository';
-import { PayableController } from './payable.controller';
 
 @Module({
-  controllers: [PayableController],
   providers: [
     PrismaService,
     {
